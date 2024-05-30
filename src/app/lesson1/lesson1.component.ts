1f53c855ee55ecaba48f3ac647db162e617cdbd8
nins-ds <155716399+nins-ds@users.noreply.github.com>
2024-05-31 05:25:43 +0800

    add lesson 3 component and add quiz on lesson1

--- src/app/lesson1/lesson1.component.ts
+++ src/app/lesson1/lesson1.component.ts
@@ -41,14 +41,54 @@ export class Lesson1Component {
 
   quizQuestions: QuizQuestion[] = [
     { 
-      text: 'What is 1 + 1?',
-      options: ['1', '2', '3', '4'],
+      text: 'It is the practice of journalism found in educational settings.',
+      options: ['Campus Journalism', 'Magazines', 'Reporting', 'Blogs'],
+      correctAnswer: 0
+    },
+    { 
+      text: 'What century did the radio, television, and the internet emerged?',
+      options: ['19th century', '20th century', '18th century', '16th century'],
       correctAnswer: 1
     },
     { 
-      text: 'What is the capital of France?',
-      options: ['London', 'Berlin', 'Madrid', 'Paris'],
+      text: 'What year did the history of college journalism in the Philippines began with the publication of El Liliputiense by the UST?',
+      options: ['1980', '1819', '1890', '1987'],
+      correctAnswer: 2
+    },
+    { 
+      text: 'It is the frequently produced by student-run publications, magazines, blogs, and radio.',
+      options: ['News', 'Blogs', 'Radio', 'Reports'],
       correctAnswer: 3
+    },
+    { 
+      text: 'What year is the history of journalism in the Philippine Islands?',
+      options: ['1890', '1964', '1896', '1933'],
+      correctAnswer: 3
+    },
+    { 
+      text: 'What year is John Lent in the Philippine Mass Communication?',
+      options: ['1962', '1890', '1964', '1933'],
+      correctAnswer: 2
+    },
+    { 
+      text: 'Campus Journalism provides student a voice to discuss important issues and events.',
+      options: ['False', 'True'],
+      correctAnswer: 1
+    },
+    { 
+      text: 'Campus Journalism helps student become more aware and engaged citizens by teaching them about current issues.',
+      options: ['False', 'True'],
+      correctAnswer: 0
+    },
+    { 
+      text: 'Schools should support campus journalism program to develop students and uphold principles of free expression and civic participation.',
+      options: ['False', 'True'],
+      correctAnswer: 1
+    },
+    { 
+      text: 'Student journalism cannot be included into a variety of courses and taught alongside disciplines like physics and history. ',
+      options: ['False', 'True'],
+      correctAnswer: 1
     },
     // Add more questions as needed
   ];
--- src/app/lesson3/lesson3.component.ts
+++ src/app/lesson3/lesson3.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TopicListService } from '../topic-list.service';
 import { QuizQuestion } from '../quiz.model';
 import { LessonQuizComponent } from '../lesson-quiz/lesson-quiz.component';
 import { ScrollToTopComponent } from '../scroll-to-top/scroll-to-top.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-lesson3',
   standalone: true,
-  imports: [LessonQuizComponent, ScrollToTopComponent],
+  imports: [LessonQuizComponent, ScrollToTopComponent, RouterLink],
   templateUrl: './lesson3.component.html',
   styleUrl: './lesson3.component.css'
 })
 export class Lesson3Component {
 
+  @Input() lessonId: string | undefined;
+  currentLessonId: string | undefined;
+
   constructor(private topicService: TopicListService) {
-    const lessonTitle = 'Lesson Title for Lesson 3';
-    const topics = ['Topic 9', 'Topic 8', 'Topic 7', 'Topic 8', 'Topic 10']; 
+    const lessonTitle = 'Types of Campus Journalism (Part 2)';
+    const topics = ['Editorial Writing', '•	Types of Editorial Writing', 
+    '•	Parts of Editorial', '•	Steps to Write Editorial Writing', '•	Structure of Editorial Writing', 
+    'Editorial Cartoon', '•	Elements of Editorial Cartoons', '•	Steps in Making Editorial Cartoons',
+    '•	Tips in Editorial Cartoon', 'Radio Broadcasting', '•	Tools for Broadcasting', '•	Tips on Writing Scripts for your Listeners', '•	Basic Steps in Radio Script Writing',
+    'Photojournalism', '•	Five Main Functions of Photos/Illustrations', '•	Characteristic of a Good Photojournalist',
+    '•	Qualities of a Good Photo', '•	What is a Caption?',
+    'Test Yourself']; 
     this.topicService.updateTopicNames(lessonTitle, topics);
+  }
+
+  ngOnInit(): void {
+    this.currentLessonId = this.lessonId;
+    console.log('in lesson1 ngOnInit, the currentLessonId is: ', this.currentLessonId);
+    
+    
   }
 
   quizQuestions: QuizQuestion[] = [
